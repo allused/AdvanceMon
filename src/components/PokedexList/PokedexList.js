5c84f8d01ff671c94d50c1f39f82ab85e82e8b85
J <J@DESKTOP-K8E0BDM>
2020-12-18 19:06:36 +0100

    next/previous page button functions added

--- src/components/PokedexList/PokedexList.js
+++ src/components/PokedexList/PokedexList.js
@@ -5,21 +5,34 @@ import PokedexBase from '../PokedexBase';
 
 function PokedexList(props) {
 
+    const[currentUrl, setCurrentUrl] = useState("https://pokeapi.co/api/v2/pokemon");
     const[pokemons, setPokemons] = useState();
     const[loading, setLoading] = useState(true);
+    const[previousUrl, setPreviousUrl] = useState("");
+    const[nextUrl, setNextUrl] = useState("");
+    
+    const nextBtnTitle = nextUrl != null ? "Next" : ""; 
+    const previousBtnTitle = previousUrl != null ? "Previous" : ""; 
+
     const pokemonListScreen = <PokemonList loading={loading} pokemons={pokemons}/>;
 
+    const nextPage = () => {nextUrl !== "" ? setCurrentUrl(nextUrl) : setNextUrl("")};
+    const previousPage = () => {previousUrl !== "" ? setCurrentUrl(previousUrl) : setPreviousUrl("")};
+
     useEffect(() => {
-        axios.get('https://pokeapi.co/api/v2/pokemon')
+        axios.get(`${currentUrl}`)
             .then(res => {
                 setPokemons(res.data.results)
                 setLoading(false)
+                setNextUrl(res.data.next)
+                setPreviousUrl(res.data.previous)
             })
-    }, [])
+    }, [currentUrl])
         
     return (
         <div>
-            <PokedexBase mainScreen={pokemonListScreen} toggleTheme={props.toggleTheme}/>
+            <PokedexBase mainScreen={pokemonListScreen} toggleTheme={props.toggleTheme} 
+            greenBtnAction={nextPage} blueBtnAction={previousPage} greenBtnTitle={nextBtnTitle} blueBtnTitle={previousBtnTitle}/>
         </div>
     )
 }
