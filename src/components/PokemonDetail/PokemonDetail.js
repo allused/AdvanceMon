6765094f429d8ee1a01912b22e011301c1d25a1d
allused <divixed12@gmail.com>
2021-02-22 16:28:45 +0100

    Added Evolution Option to the Option Controller as the second option prop

--- src/components/PokemonDetail/PokemonDetail.js
+++ src/components/PokemonDetail/PokemonDetail.js
@@ -3,10 +3,11 @@ import { useEffect, useState, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import { makeStyles } from '@material-ui/styles';
 import OverviewOption from '../PokemonDetailMenu/OverviewOption';
+import OptionsContainer from '../PokemonDetailMenu/OptionsContainer';
+import EvolutionOption from '../PokemonDetailMenu/EvolutionOptionMenu';
 import PokemonDetailMainScreen from '../PokemonDetailMainScreen/OverviewOptionContent';
 import PokemonDetailStats from '../PokemonDetail/PokemonDetailStats';
 import PokedexBase from '../PokedexBase';
-import OptionsContainer from '../PokemonDetailMenu/OptionsContainer'
 
 /* 
 This component is responsible for the Pokemon Detail page, it calls the Pokedex Base and the Pokemon detail related components
@@ -18,9 +19,12 @@ function PokemonDetail(props) {
     let {id} = useParams();
     const[pokemon, setPokemon] = useState([]);
     const[loading, setLoading] = useState(true);
+
     const overviewOption = <OverviewOption />
+    const evolutionOption = <EvolutionOption />
+
     const mainScreen =  <PokemonDetailMainScreen pokemon={pokemon}/>
-    const optionMenu =  <OptionsContainer firstOption={overviewOption}/>
+    const optionMenu =  <OptionsContainer firstOption={overviewOption} secondOption={evolutionOption}/>
     const statScreen = <PokemonDetailStats pokemon={pokemon}/>
 
     const useStyle = makeStyles({
